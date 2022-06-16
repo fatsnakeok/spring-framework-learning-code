336edcb865d41a32c0fc7df2d03e0c5340750e0d
liuxin <liuxin@izaodao.org>
2022-06-16 09:27:32 +0800

    使用反射创建bean，取代new方式。

--- spring_00_introdution/src/main/java/com/fatsnake/architecture/c_reflect/servlet/DemoServlet3.java
+++ spring_00_introdution/src/main/java/com/fatsnake/architecture/c_reflect/servlet/DemoServlet3.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/demo2")
+@WebServlet(urlPatterns = "/demo3")
 public class DemoServlet3 extends HttpServlet {
     
     DemoService demoService = BeanFactory.getDemoService();
